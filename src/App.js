c16c077a8c566cfa8b4a1429c62406864925e21c
IamEssJay <emmasj2019@gmail.com>
2023-04-18 19:36:26 +0100

    Update todo list Completed or not?

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import{AiOutlinePlus} from 'react-icons/ai';
 import Todo from "./Todo";
 import {db} from "./Firebase";
-import { query,collection, onSnapshot} from "firebase/firestore";
+import { query,collection, onSnapshot, updateDoc, doc} from "firebase/firestore";
 const style={
   bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
   container:`bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
@@ -32,6 +32,12 @@ function App() {
     return()=> unsubscribe()
   },[])
   // U todo
+  const toggleComplete= async(todo)=>{
+    await updateDoc(doc(db, 'todo', todo.id), {
+      completed: !todo.completed
+    })
+
+  }
   // D todo
 
   return (
@@ -44,7 +50,7 @@ function App() {
       </form>
       <ul>
         {todos.map((todo, index)=>(
-           <Todo key={index} todo={todo}/>
+           <Todo key={index} todo={todo} toggleComplete={toggleComplete}/>
           )
         )}
       </ul>
